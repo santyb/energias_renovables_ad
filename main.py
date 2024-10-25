08293f71c309a12ebecb92bc505ed1de12f19fb2
Santiago B <santyb0126@hotmail.com>
2024-10-24 20:45:36 -0500

    add map

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ from datetime import datetime
 import altair as alt
 import plotly.express as px
 import numpy as np
+import pydeck
 
 #Leer archivo csv
 def cargar_archivo():
-    data = pd.read_csv('Meta_FNCER__Incorporar_en_la_matriz_energ_tica_nueva_capacidad_instalada_a_partir_de_Fuentes_No_Convencionales_de_Energ_a_Renovable_-_FNCER_20241003.csv')
+    data = pd.read_csv('data/Meta_FNCER__Incorporar_en_la_matriz_energ_tica_nueva_capacidad_instalada_a_partir_de_Fuentes_No_Convencionales_de_Energ_a_Renovable_-_FNCER_20241003.csv')
     return data
 
+def cargar_posicion():
+    data = pd.read_excel('data/departamentos.xlsx', sheet_name='Departamentos')
+    return data
 
+df_posicion = cargar_posicion()
 df_energias_renovables = cargar_archivo()
-
+df_energias_renovables['Fecha estimada FPO'] = df_energias_renovables['Fecha estimada FPO'].str[:10]
+df_energias_renovables.columns = df_energias_renovables.columns.str.replace(' ', '_')
+df_energias_renovables = pd.merge(df_energias_renovables, df_posicion[['Código_Departamento', 'LATITUD', 'LONGITUD']], on='Código_Departamento', how='left')
+df_energias_renovables = df_energias_renovables.rename(columns={'Energía_[kWh/día]': 'Energía_kWh_día'})
+df_energias_renovables = df_energias_renovables.rename(columns={'Emisiones_CO2_[Ton/año]': 'Emisiones_CO2_Ton_año'})
 st.title('Energía Renovable Sostenible y Eficiente')
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Datos", "Proyectos Por Departamento", "Tipo de energia", "Mapa", "Circular", "Histograma"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Datos", "Proyectos Por Departamento", "Tipo de energia", "Mapa", "Energia Vs CO2"])
 
 with tab1:
     ver_df = st.toggle('Ver DataFrame', value=True)
@@ -51,23 +60,53 @@ with tab3:
         st.pyplot(fig)
         
 with tab4:
-    
-    department_energy = df_energias_renovables.groupby(['Departamento']).Energia.sum().reset_index()
 
-    # Show results
-    st.write("Total Energy by Department:")
-    st.write(department_energy)
+ # Mapa  1
 
-    # Optionally, show a bar chart
-    st.bar_chart(department_energy.set_index('Departamento'))
-    
-with tab5:
-    chart_data = pd.DataFrame(
-        {
-            "col1": list(range(20)) * 3,
-            "col2": np.random.randn(60),
-            "col3": ["A"] * 20 + ["B"] * 20 + ["C"] * 20,
-        }
+    df_energia_departamento_mapa = df_energias_renovables.groupby(by=['Departamento', 'LATITUD','LONGITUD'], as_index=False).Energía_kWh_día.sum()
+    df_energia_departamento_mapa['size'] = df_energia_departamento_mapa['Energía_kWh_día'] * 0.008
+    with st.container(border=True):
+        st.header('Mapa de energia generada')
+        st.map(df_energia_departamento_mapa, latitude='LATITUD', longitude='LONGITUD', size='size')
+
+    capas=pydeck.Layer(
+        "ScatterplotLayer",
+        data=df_energia_departamento_mapa,
+        get_position=["LONGITUD", "LATITUD"],
+        get_color="[255, 75, 75]",
+        pickable=True,
+        auto_highlight=True,
+        get_radius="size"
     )
 
-    st.bar_chart(chart_data, x="col1", y="col2", color="col3")
+    vista_inicial=pydeck.ViewState(
+        latitude=4,
+        longitude=-74,
+        zoom=4.5,
+    )
+
+    with st.container(border=True):
+        st.header('Mapa de energia generada')
+        st.pydeck_chart(
+            pydeck.Deck(
+                layers=capas,
+                map_style=None,
+                initial_view_state=vista_inicial,
+                tooltip={"text": "{Departamento}\nEnergia_generada: {Energía_kWh_día}"}
+            )
+        )
+
+with tab5:
+    energia_generada = df_energias_renovables.groupby('Departamento').agg({'Energía_kWh_día': 'sum'})
+    co2_generad0 = df_energias_renovables.groupby('Departamento').agg({'Emisiones_CO2_Ton_año': 'sum'})
+    print('esta es la energia generada', energia_generada)
+
+    resultado = df_energias_renovables.groupby('Departamento')[['Energía_kWh_día', 'Emisiones_CO2_Ton_año']].sum().reset_index()
+    print('esta es el df', resultado)
+
+    # Gráfico de barras
+    fig = px.bar(resultado, x='Departamento', y=['Energía_kWh_día', 'Emisiones_CO2_Ton_año'],
+                title='Generacion de energia vs co2', barmode='group')
+
+    # Mostrar el gráfico en Streamlit
+    st.plotly_chart(fig)
